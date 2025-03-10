22311c17319bddef39f67587ce650183692be0c1
claudiatriozzi <blackmelantha@gmail.com>
2025-03-10 19:53:21 +0100

    new slideshow

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let slideIndex = 0;
+    const images = document.querySelectorAll('.image-container img');
+
+    showSlides();
+
+    // Fonction pour avancer et reculer les images
+    function plusSlides(n) {
+        slideIndex += n;
+        showSlides();
+    }
+
+    // Fonction pour afficher l'image correspondant à l'index actuel
+    function showSlides() {
+        // Vérifier que les images sont bien sélectionnées
+        if (!images || images.length === 0) return;
+
+        // Masquer toutes les images
+        images.forEach(img => img.classList.remove('show'));
+
+        // Vérifier les limites de l'index
+        if (slideIndex >= images.length) {
+            slideIndex = 0;
+        } else if (slideIndex < 0) {
+            slideIndex = images.length - 1;
+        }
+
+        // Afficher l'image actuelle
+        images[slideIndex].classList.add('show');
+    }
+
+    // Ajouter les événements de navigation
+    document.querySelector('.prev').addEventListener('click', function() {
+        plusSlides(-1);
+    });
+    document.querySelector('.next').addEventListener('click', function() {
+        plusSlides(1);
+    });
+});
